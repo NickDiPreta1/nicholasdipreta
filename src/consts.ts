914f2ce32b08860af54a4187689965dd9e19aeb8
Nicholas DiPreta <nicholasdipreta@gmail.com>
2026-02-03 21:06:50 -0500

    chore: cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Responsibility in Software';
-export const SITE_DESCRIPTION = 'Welcome to my website and blog';
+export const SITE_DESCRIPTION = 'Exploring ethical responsibility in software systems — a blog by Nicholas DiPreta.';
